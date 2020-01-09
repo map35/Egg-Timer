e2ab4b5ce9682f62063a38ac45f118fb385a28ba
Mohammad Agung <diditkazama@gmail.com>
2020-01-09 15:50:43 +0700

    tambah var counter, update fungsi kematangan, tambah fungsi timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  EggTimer
 //
-//  Created by Angela Yu on 08/07/2019.
+//  Created by Mohammad Agung Pambudi on 08/07/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
@@ -10,18 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTime = ["Soft": 5, "Medium": 8, "Hard": 12]
+    let eggTime = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var counter = 10
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
+        let hardness = sender.currentTitle!
         
-        if hardness == "Soft" {
-            print(eggTime["Soft"])
-        } else if hardness == "Medium" {
-            print(eggTime["Medium"])
-        } else if hardness == "Hard" {
-            print(eggTime["Hard"])
-        }
+        counter = eggTime[hardness]!
+        
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
+    @objc func updateTimer() {
+        if counter > 0 {
+            print(counter)
+            counter -= 1
+        }
+    }
+
 }
